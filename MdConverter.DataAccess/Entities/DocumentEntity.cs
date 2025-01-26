a839886d74611f74ab40ed1dff79f1a479c97178
Yuizhey <Gre4ka28@yandex.ru>
2025-01-26 20:14:48 +0300

    обновил миграции

--- MdConverter.DataAccess/Entities/DocumentEntity.cs
+++ MdConverter.DataAccess/Entities/DocumentEntity.cs
@@ -4,7 +4,6 @@ public class DocumentEntity
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    // public string Url { get; set; }
     public UserEntity User { get; set; }
     public Guid UsertId { get; set; }
 }
