ace83de395434daa8084292a4a961d65a5fee58d
Yuizhey <Gre4ka28@yandex.ru>
2025-02-03 00:00:13 +0300

    доделал конфигурацию из переменного окружения

--- MdConverter.Api/Program.cs
+++ MdConverter.Api/Program.cs
@@ -14,25 +14,46 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // Заменяем загрузку конфигурации по умолчанию (appsettings.json) 
+        builder.Configuration.AddEnvironmentVariables();
+
         // Add services to the container.
         builder.Services.AddAuthorization();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        // Configure Swagger
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        
+        // Настройка DbContext с учетом переменных окружения
+        var connectionString = builder.Configuration["ConnectionStrings:MdConverterDbContext"];
         builder.Services.AddDbContext<MdConverterDbContext>(
-            options => options.UseNpgsql(builder.Configuration.GetConnectionString("MdConverterDbContext")));
+            options => options.UseNpgsql(connectionString));
+        
+        // Регистрация сервисов и репозиториев
         builder.Services.AddScoped<IUserRepository, UserRepository>();
         builder.Services.AddScoped<IAccountService, AccountService>();
         builder.Services.AddScoped<IDocumentService, DocumentService>();
         builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
         builder.Services.AddScoped<JwtService>();
-        builder.Services.Configure<AuthSettings>(builder.Configuration.GetSection("AuthSettings"));
-        builder.Services.Configure<MinioSettings>(builder.Configuration.GetSection("MinioSettings"));
-        builder.Services.AddSingleton<MinioService>(); 
+
+        // Настройка параметров аутентификации и Minio через переменные окружения
+        builder.Services.Configure<AuthSettings>(options =>
+        {
+            options.SecretKey = builder.Configuration["AUTH:SECRETKEY"];
+            options.Expires = TimeSpan.Parse(builder.Configuration["AUTH:EXPIRES"]!);
+        });
+
+        builder.Services.Configure<MinioSettings>(options =>
+        {
+            options.Endpoint = builder.Configuration["MINIO:URL"];
+            options.AccessKey = builder.Configuration["MINIO:ACCESSKEY"];
+            options.SecretKey = builder.Configuration["MINIO:SECRETKEY"];
+            options.BucketName = builder.Configuration["MINIO:BUCKETNAME"];
+        });
+        
+        builder.Services.AddSingleton<MinioService>();
         builder.Services.AddControllers();
         builder.Services.AddAuth(builder.Configuration);
-        
 
         var app = builder.Build();
 
@@ -50,5 +71,7 @@ public class Program
         app.UseAuthorization();
 
         app.Run();
+        Console.WriteLine(builder.Configuration["AUTH:SECRETKEY"]);
+        Console.WriteLine(TimeSpan.Parse(builder.Configuration["AUTH:EXPIRES"]));
     }
 }
